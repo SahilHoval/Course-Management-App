e6c24fa7e005e1e5272ca3ade432ca535cefaf00
SahilHoval <129523886+SahilHoval@users.noreply.github.com>
2023-06-09 23:44:53 +0530

    Add files via upload

---
+++ course_app/src/testjavacomcourse_management_appcourse_app/CourseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course_management_app.course_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
